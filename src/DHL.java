e39d40cfe6582c4f413e1a6b9c5889f8523cf387
Larysa <onilarisa@gmail.com>
2025-02-11 19:12:20 +0100

    initial Commit

---
+++ src/DHL.java
@@ -0,0 +1,7 @@
+public class DHL implements MailDeliveryService{
+    @Override
+    public void sendMail() {
+        System.out.println("Положи письмо в конверт, поставь марку, отправь");
+
+    }
+}
